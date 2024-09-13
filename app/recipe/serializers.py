e17914457a206f866b8b704885be7c9cc9dbe96a
Yi Ouyang <oyy994@gmail.com>
2024-09-13 16:02:31 +1000

    Created Ingredient  module.

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -20,9 +20,7 @@ class RecipeSerializer(serializers.ModelSerializer):
         fields = ['id', 'title', 'time_minutes', 'price', 'link', 'tags']
         read_only_fields = ['id', ]  # id字段只读
 
-    def create(self, validated_data):
-        tags = validated_data.pop('tags', [])  # 获取标签
-        recipe = Recipe.objects.create(**validated_data)
+    def _get_or_create_tags(self, tags, recipe):
         auth_user = self.context['request'].user
         for tag in tags:
             tag_obj, created = Tag.objects.get_or_create(
@@ -31,7 +29,24 @@ class RecipeSerializer(serializers.ModelSerializer):
             )
             recipe.tags.add(tag_obj)
 
+    def create(self, validated_data):
+        tags = validated_data.pop('tags', [])  # 获取标签
+        recipe = Recipe.objects.create(**validated_data)
+        self._get_or_create_tags(tags, recipe)
+
         return recipe
+
+    def update(self, instance, validated_data):
+        tags = validated_data.pop('tags', None)
+        if tags is not None:
+            instance.tags.clear()
+            self._get_or_create_tags(tags, instance)
+
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+
+        instance.save()
+        return instance
 
 
 class RecipeDetailSerializer(RecipeSerializer):
